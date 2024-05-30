53e996592c393c394002f5897bb8b2beeb27447e
SalihMorina123 <146649620+SalihMorina123@users.noreply.github.com>
2024-05-30 22:09:30 +0200

    Add files via upload

---
+++ AsynkKlient/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace AsynkKlient
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+     
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+    
+        private void InitializeComponent()
+        {
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(311, 349);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMessage.TabIndex = 0;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(417, 346);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(252, 136);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(300, 150);
+            this.textBoxLog.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ AsynkKlient/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsynkKlient
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient();
+                await client.ConnectAsync("127.0.0.1", 5000);
+                stream = client.GetStream();
+                textBoxLog.AppendText("Connected to server...\r\n");
+
+                _ = Task.Run(async () =>
+                {
+                    try
+                    {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                        {
+                            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                            Invoke((Action)(() => textBoxLog.AppendText($"Received: {message}\r\n")));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Invoke((Action)(() => textBoxLog.AppendText($"Error: {ex.Message}\r\n")));
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                textBoxLog.AppendText($"Error: {ex.Message}\r\n");
+            }
+        }
+
+        private async void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = textBoxMessage.Text;
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(data, 0, data.Length);
+                textBoxLog.AppendText($"Sent: {message}\r\n");
+                textBoxMessage.Clear();
+            }
+            catch (Exception ex)
+            {
+                textBoxLog.AppendText($"Error: {ex.Message}\r\n");
+            }
+        }
+
+    }
+}
